4ac53c5a84dcffcee888daecf6ae72cd815bae7a
huangjinhe <306059358@qq.com>
2018-12-28 11:05:35 +0800

    他

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.111111111111";
 
             return View();
         }
